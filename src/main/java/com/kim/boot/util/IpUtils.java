bfff982ae4889b9394003ba6e4bec27eae17656d
duanledexianxianxian <fengyuchenglun@foxmail.com>
2020-05-15 13:32:08 +0800

    init

---
+++ src/main/java/com/kim/boot/util/IpUtils.java
@@ -0,0 +1,37 @@
+package com.kim.boot.util;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * ip地址相关工具类
+ *
+ * @author duanledexianxianxian
+ * @version 1.0.0
+ */
+public class IpUtils {
+
+    private static final String UNKNOWN = "unknown";
+
+    /**
+     * 获取IP地址
+     * 使用Nginx等反向代理软件， 则不能通过request.getRemoteAddr()获取IP地址
+     * 如果使用了多级反向代理的话，X-Forwarded-For的值并不止一个，而是一串IP地址，X-Forwarded-For中第一个非unknown的有效IP字符串，则为真实IP地址
+     *
+     * @param request the request
+     * @return the ip addr
+     */
+    public static String getIpAddr(HttpServletRequest request) {
+
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || UNKNOWN.equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || UNKNOWN.equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || UNKNOWN.equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return "0:0:0:0:0:0:0:1".equals(ip) ? "127.0.0.1" : ip;
+    }
+}
---
+++ src/main/java/com/kim/boot/util/NumberUtils.java
@@ -0,0 +1,38 @@
+package com.kim.boot.util;
+
+/**
+ * The type Number utils.
+ *
+ * @author duanledexianxianxian
+ * @version 1.0.0
+ */
+public class NumberUtils {
+
+    /**
+     * 字符串转整形
+     *
+     * @param str the str
+     * @return the long
+     */
+    public static Long toLongNull(final String str) {
+        return toLong(str, null);
+    }
+
+    /**
+     * 字符串转整形
+     *
+     * @param str          the str
+     * @param defaultValue the default value
+     * @return long long
+     */
+    public static Long toLong(final String str, final Long defaultValue) {
+        if (str == null) {
+            return defaultValue;
+        }
+        try {
+            return Long.parseLong(str);
+        } catch (final NumberFormatException nfe) {
+            return defaultValue;
+        }
+    }
+}
